2ffc6e60d504b6130f0c225a409f4040c2685929
Crowdbotics <team@crowdbotics.com>
2021-08-05 09:12:35 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "fddsf_29497Identifier",
+  url: "https://fddsf_29497Identifier.botics.co",
+  api: "https://fddsf_29497Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
